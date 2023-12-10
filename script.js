31ef6cfb3a02a8da035a5dab3338c14b7589512b
Valentin <93677277+Vali644@users.noreply.github.com>
2023-12-10 20:31:30 +0200

    Add basic score functionality

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
+
+
 document.querySelector("#rock").addEventListener("click", ()=>{play("rock", getComputerChoice())});
-
 document.querySelector("#paper").addEventListener("click", ()=>{play("paper", getComputerChoice())});
+document.querySelector("#scissors").addEventListener("click", ()=>{play("scissors", getComputerChoice())});
 
-document.querySelector("#scissors").addEventListener("click", ()=>{play("scissors", getComputerChoice())})
 function getComputerChoice () {
     randomN = Math.floor(Math.random() * 3);
     switch (randomN) {
@@ -15,6 +16,10 @@ function getComputerChoice () {
     }
 }
 
+let playerScore = 0;
+
+let computerScore = 0;
+
 function play (playerSelection, computerSelection) {
     playerSelection = playerSelection.toLowerCase();
     if (playerSelection == "rock") {
@@ -24,36 +29,45 @@ function play (playerSelection, computerSelection) {
                 break;
             case "Paper":
                 document.getElementById("result").innerText = "You loose! Paper beats Rock";
+                computerScore += 1;
                 break;
             case "Scissors":
                 document.getElementById("result").innerText = "You win! Rock beats Scissors";
+                playerScore += 1;
                 break;
         }
     } else if (playerSelection == "paper") {
         switch (computerSelection) {
             case "Rock":
                 document.getElementById("result").innerText = "You Win! Paper beats Rock";
+                playerScore += 1;
                 break;
             case "Paper":
                 document.getElementById("result").innerText = "Tie! Both choose Paper";
                 break;
             case "Scissors":
                 document.getElementById("result").innerText = "You loose! Scissors beats Paper";
+                computerScore += 1;
                 break;
         }
     } else if (playerSelection = "scissors") {
         switch (computerSelection) {
             case "Rock":
                 document.getElementById("result").innerText = "You loose! Rock beats Scissors";
+                computerScore += 1;
                 break;
             case "Paper":
                 document.getElementById("result").innerText = "You Win! Scissors beats Paper";
+                playerScore += 1;
                 break;
             case "Scissors":
                 document.getElementById("result").innerText = "Tie! Both chose Scissors";
                 break;
         }
     }
+    // left to implement a 5 point score cap
+    document.querySelector("#playerScore").innerText = playerScore;
+    document.querySelector("#computerScore").innerText = computerScore;
 }
 
 function game () {
